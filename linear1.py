705da61d287b969d78a6c346ff477fac41e99367
Venkat Krishna <codekvkk@gmail.com>
2024-10-10 21:37:34 +0530

    saved first.py and linear1.py

---
+++ linear1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as mpl
+
+#x_train are the input featurea
+#y_train are the output targets
+
+x_train=np.array([1.0,2.0])
+y_train=np.array([300.00,500.00])
+print(f"x_train={x_train}")
+print(f"y_train={y_train}")
+
+
